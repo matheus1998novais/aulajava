e0273b288bf1d5fb3fc1aaa16f64d0b1f8f08968
MATHEUS ALVES NOVAIS <matheus.anovais1@senacsp.edu.br>
2025-07-16 17:41:36 -0300

    comit

---
+++ src/Aula03/mediaaluno.java
@@ -0,0 +1,16 @@
+package Aula03;
+
+public class mediaaluno {
+    public static void main(String[] args) {
+        double n1 = 8;
+        double n2 = 5;
+        double n3 = 7;
+        double n4 = 8;
+        double media = (n1 + n2 + n3 + n4)/4;
+
+        System.out.println ("resultado; " + media );
+
+    }
+
+
+}
